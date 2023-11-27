b6bf62caa97cf3e38840aeea17affd586c03b104
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-11-26 23:00:52 -0500

    added state management to slider component

--- client/src/components/synth_components/SynthSlider.jsx
+++ client/src/components/synth_components/SynthSlider.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
+
 import * as Slider from "@radix-ui/react-slider";
 
 import "./SynthSlider.css"
 
 export default function SynthSlider() {
+
+  const [value, setValue] = useState(50)
+
   return (
     <form className="w-100">
+      <p>{value}</p>
       <Slider.Root
         className="SliderRoot"
-        defaultValue={[50]}
+        value={[value]}
         max={100} 
         min={1}
-        step={1}
+        step={1} 
+        onValueChange={(newValue) => setValue(newValue)}
+
       >
         <Slider.Track className="SliderTrack">
           <Slider.Range className="SliderRange" />
